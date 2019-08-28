a2ceaa9d9bb3f4016f3ebda16f456735f2b6c35c
Erik <eeckee@gmail.com>
2019-08-28 12:47:43 +0200

    robot rename fixed wavs to original dir name

--- Robot/fix_wavs.py
+++ Robot/fix_wavs.py
@@ -1,6 +1,7 @@
 from os.path import join, basename
 from os import makedirs, listdir
 from subprocess import check_output
+import shutil
 
 
 path = "data/training_set/audio"
@@ -10,3 +11,8 @@ makedirs(new_path)
 
 for fname in listdir(path):
     check_output(f"sox {join(path,fname)} -r 16000 {join(new_path, fname)}", shell=True)
+
+ans = input("remove old files? (y/n)")
+if ans.lower() == "y":
+    shutil.rmtree(path)
+    shutil.move(new_path, path)
